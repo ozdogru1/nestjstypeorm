9079d6ad959c0124aceecb6994e564bc4cbd4ab8
Yunus <yunusac51@gmail.com>
2026-03-18 17:28:21 +0300

    fix: maints fixed

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
