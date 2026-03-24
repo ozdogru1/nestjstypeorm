ef7bcd1f40a7ae76bb62270d480d116b44db1ec0
Yunus <yunusac51@gmail.com>
2026-03-24 16:51:33 +0300

    feat: dockerfile added and some files changed

--- src/auth/dto/auth-login.dto.ts
+++ src/auth/dto/auth-login.dto.ts
@@ -2,9 +2,9 @@ import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class AuthLoginDto {
   @IsEmail()
-  email: string;
+  email!: string;
 
   @IsString()
   @MinLength(6)
-  password: string;
+  password!: string;
 }
